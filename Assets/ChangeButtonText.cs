91fe5360576fcf1804d0bd8b3d1373d4f2429bf1
Justin Wilkinson <wilkinsense@gmail.com>
2016-02-08 12:25:49 -0500

    Initial commit covering all of the work we did in-class.

---
+++ Assets/ChangeButtonText.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeButtonText : MonoBehaviour {
+
+  // Button we are going to change the text of.
+  public Button button;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+  public void ChangeText() {
+    if(button) {
+      // Gets the Text child object of the button so that we can set its text.
+      Text buttonText = button.GetComponentInChildren<Text>();
+      buttonText.text = "Pizza";
+    }
+  }
+}
