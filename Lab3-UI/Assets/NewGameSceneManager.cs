7f4bee639cd3375b9970188178723229e9fb5212
skyeturriff <turriff.skye@gmail.com>
2016-02-08 13:22:52 -0500

    Add Lab3

---
+++ Lab3-UI/Assets/NewGameSceneManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class NewGameSceneManager : MonoBehaviour {
+
+  public Slider valueSlider;
+  string sliderKey = "VALUE_SLIDER_INITIAL_VALUE";
+
+  // Use this for initialization
+  void Start () {
+    if(PlayerPrefs.HasKey(sliderKey)) {
+      valueSlider.value = PlayerPrefs.GetFloat(sliderKey);
+    }
+    else {
+      PlayerPrefs.SetFloat(sliderKey, valueSlider.value);
+      PlayerPrefs.Save();
+    }
+  }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+  public void SaveSliderValue() {
+    // Will create new/override existing value.
+    PlayerPrefs.SetFloat(sliderKey, valueSlider.value);
+    PlayerPrefs.Save();
+  }
+}
